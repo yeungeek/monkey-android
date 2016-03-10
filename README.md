49ce76667f6fe79eee2fe63814d42310ac8e8408
yeungeek <yangjian0410@gmail.com>
2016-03-10 10:45:13 +0800

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Monkey for GitHub, Android Version
-### 
+是一个Github第三方客户端，可以查看最新的repo，user，trending。Ios版本的在这里[Monkey](https://github.com/coderyi/Monkey)
+
+## Function
+* 登陆 ~~删除~~
+* user列表
+* repo列表
+* trending(daily,weekly,monthly)
+* user详情
+* repo详情
+* follow user
+* star repo
+* 搜索
+## API
+主要使用到的API列表
+## Screenshots
+
+## Open source projects
+* [RxJava](https://github.com/ReactiveX/RxJava/)
+* [RxAndroid](https://github.com/ReactiveX/RxAndroid/)
+* [retrofit](https://github.com/square/retrofit/)
+* [OkHttp](https://github.com/square/okhttp)
+* [Dagger2](https://github.com/google/dagger/)
+* [glide](https://github.com/bumptech/glide/)
+* [ButterKnife](https://github.com/JakeWharton/butterknife/)
+* [Stetho](https://github.com/facebook/stetho)
+* [Sqlbrite](https://github.com/square/sqlbrite)
+* [Timber](https://github.com/JakeWharton/timber/)
--- app/src/main/java/com/yeungeek/monkeyandroid/ui/MainActivity.java
+++ app/src/main/java/com/yeungeek/monkeyandroid/ui/MainActivity.java
@@ -1,29 +1,18 @@
 package com.yeungeek.monkeyandroid.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.yeungeek.monkeyandroid.R;
 import com.yeungeek.monkeyandroid.ui.base.view.BaseActivity;
-import com.yeungeek.monkeyandroid.ui.sample.SampleActivity;
 
 import butterknife.Bind;
 
 public class MainActivity extends BaseActivity {
     @Bind(R.id.toolbar)
     Toolbar toolbar;
-    @Bind(R.id.fab)
-    FloatingActionButton fab;
-    @Bind(R.id.button_sample)
-    Button sampleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +20,8 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         setSupportActionBar(toolbar);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
-        sampleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ActivityCompat.startActivity(MainActivity.this, new Intent(MainActivity.this, SampleActivity.class), null);
-            }
-        });
+
     }
 
     @Override
@@ -56,12 +33,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
