2c192e9c34fbca42b7b2117ff2b1512db9891249
yeungeek <yangjian0410@gmail.com>
2016-04-12 19:53:35 +0800

    update repo detail

--- app/src/main/java/com/yeungeek/monkeyandroid/ui/detail/DetailActivity.java
+++ app/src/main/java/com/yeungeek/monkeyandroid/ui/detail/DetailActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.MenuItem;
 
 import com.yeungeek.monkeyandroid.R;
 import com.yeungeek.monkeyandroid.data.model.Repo;
@@ -35,6 +36,17 @@ public class DetailActivity extends BaseActivity {
 
         serializable = getIntent().getSerializableExtra(EXTRA_DETAIL);
         initViews();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initViews() {
--- app/src/main/java/com/yeungeek/monkeyandroid/ui/detail/RepoDetailFragment.java
+++ app/src/main/java/com/yeungeek/monkeyandroid/ui/detail/RepoDetailFragment.java
@@ -3,16 +3,43 @@ package com.yeungeek.monkeyandroid.ui.detail;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.yeungeek.monkeyandroid.R;
 import com.yeungeek.monkeyandroid.data.model.Repo;
 import com.yeungeek.monkeyandroid.ui.base.view.BaseFragment;
 import com.yeungeek.monkeyandroid.util.AppCst;
 
+import butterknife.Bind;
+
 /**
  * Created by yeungeek on 2016/4/10.
  */
 public class RepoDetailFragment extends BaseFragment {
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.id_repo_owner_avatar)
+    ImageView mAvatar;
+    @Bind(R.id.id_repo_name)
+    TextView mRepoName;
+    @Bind(R.id.id_repo_stars)
+    TextView mStar;
+    @Bind(R.id.id_repo_fork)
+    TextView mFork;
+    @Bind(R.id.id_repo_desc)
+    TextView mRepoDesc;
+
+    private ActionBar actionBar;
+
+    private Repo mRepo;
+    private String mStarString;
+    private String mForkString;
+
     public static Fragment newInstance(final Context context, final Repo repo) {
         Bundle bundle = new Bundle();
         bundle.putSerializable(AppCst.EXTRA_REPO, repo);
@@ -20,7 +47,44 @@ public class RepoDetailFragment extends BaseFragment {
     }
 
     @Override
+    protected void init() {
+        super.init();
+        mRepo = (Repo) getArguments().getSerializable(AppCst.EXTRA_REPO);
+        mStarString = getString(R.string.title_star);
+        mForkString = getString(R.string.title_fork);
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        if (null == mRepo) {
+            return;
+        }
+
+        if (null != toolbar) {
+            ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+            actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+            if (null != actionBar) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back);
+                actionBar.setTitle(mRepo.getName());
+            }
+        }
+
+        mRepoName.setText(mRepo.getFull_name());
+        mStar.setText(String.format(mStarString, mRepo.getStargazers_count()));
+        mFork.setText(String.format(mForkString, mRepo.getForks_count()));
+        mRepoDesc.setText(mRepo.getDescription());
+
+        if (null != mRepo.getOwner()) {
+            Glide.with(this).load(mRepo.getOwner().getAvatarUrl()).into(mAvatar);
+        }
+    }
+
+    @Override
     protected int getLayoutRes() {
         return R.layout.fragment_repo_detail;
     }
+
+
 }
